b2394f7767ee8d4c4517cfb8f54cde932c1a7ca5
amir <amir@dragonchain.com>
2023-01-30 13:14:17 +0100

    match formatters

--- prisma/seeds/seed.ts
+++ prisma/seeds/seed.ts
@@ -1,76 +1,76 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 import * as bcrypt from "bcrypt";
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 async function main() {
-    const amir = await prisma.user.upsert({
-        where: { email: "amir@prisma.io" },
-        update: {
-            Profile: {
-                create: {
-                    fname: "Amir",
-                    lname: "Ben Naser",
-                    dob: new Date(),
-                    country: "Sokra",
-                    bio: "I am gay !",
-                    avatar: "monkey"
-                }
-            }
-        },
+  const amir = await prisma.user.upsert({
+    where: { email: "amir@prisma.io" },
+    update: {
+      Profile: {
         create: {
-            email: 'amir@prisma.io',
-            username: 'Amir',
-            password: await bcrypt.hash("Samir", 10),
-            Profile: {
-                create: {
-                    fname: "Amir",
-                    lname: "Ben Naser",
-                    dob: new Date(),
-                    country: "Sokra",
-                    bio: "I am gay !",
-                    avatar: "monkey"
-                }
-            }
+          fname: "Amir",
+          lname: "Ben Naser",
+          dob: new Date(),
+          country: "Sokra",
+          bio: "I am gay !",
+          avatar: "monkey",
         },
-    })
-    const chams = await prisma.user.upsert({
-        where: { email: 'chams@prisma.io' },
-        update: {
-            Profile: {
-                create: {
-                    fname: "Chamsdine",
-                    lname: "El Benna",
-                    dob: new Date(),
-                    country: "Ariana",
-                    bio: "I am not gay !",
-                    avatar: "monkey"
-                }
-            }
-        },
+      },
+    },
+    create: {
+      email: "amir@prisma.io",
+      username: "Amir",
+      password: await bcrypt.hash("Samir", 10),
+      Profile: {
         create: {
-            email: 'chams@prisma.io',
-            username: 'Chams',
-            password: await bcrypt.hash("Samir", 10),
-            Profile: {
-                create: {
-                    fname: "Chamsdine",
-                    lname: "El Benna",
-                    dob: new Date(),
-                    country: "Ariana",
-                    bio: "I am not gay !",
-                    avatar: "monkey"
-                }
-            }
+          fname: "Amir",
+          lname: "Ben Naser",
+          dob: new Date(),
+          country: "Sokra",
+          bio: "I am gay !",
+          avatar: "monkey",
         },
-    })
-    console.log({ amir, chams })
+      },
+    },
+  });
+  const chams = await prisma.user.upsert({
+    where: { email: "chams@prisma.io" },
+    update: {
+      Profile: {
+        create: {
+          fname: "Chamsdine",
+          lname: "El Benna",
+          dob: new Date(),
+          country: "Ariana",
+          bio: "I am not gay !",
+          avatar: "monkey",
+        },
+      },
+    },
+    create: {
+      email: "chams@prisma.io",
+      username: "Chams",
+      password: await bcrypt.hash("Samir", 10),
+      Profile: {
+        create: {
+          fname: "Chamsdine",
+          lname: "El Benna",
+          dob: new Date(),
+          country: "Ariana",
+          bio: "I am not gay !",
+          avatar: "monkey",
+        },
+      },
+    },
+  });
+  console.log({ amir, chams });
 }
 main()
-    .then(async () => {
-        await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-    })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
