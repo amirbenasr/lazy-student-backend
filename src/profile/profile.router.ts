52f3a022495ff314c31c75242c14e83defb9fffa
amir <amir@dragonchain.com>
2023-02-06 14:30:36 +0100

    fix image upload

--- src/profile/profile.router.ts
+++ src/profile/profile.router.ts
@@ -2,13 +2,13 @@ import express from "express";
 
 import type { Request, Response } from "express";
 
-import multer from 'multer';
+import multer from "multer";
 import { body, validationResult } from "express-validator";
 import { verifyToken } from "../middlewares";
 import * as ProfileService from "./profile.service";
 import jwtDecode from "jwt-decode";
 import { Profile } from "../profile.type";
-import fs from 'fs';
+import fs from "fs";
 
 export const profileRouter = express.Router();
 
@@ -38,65 +38,54 @@ profileRouter.get("/public/:username", async (req: Request, res: Response) => {
   }
 });
 
-
 profileRouter.put("/", verifyToken, async (req: Request, res: Response) => {
   const userId = res.locals.id;
 
   const userProfile = await ProfileService.findProfileById(userId);
   const username = userProfile?.user.username;
 
-  console.log(userProfile)
-  var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-
-      var balayAudPath = 'uploads/' + username + '/profile'
-
-      fs.mkdirSync(balayAudPath, { recursive: true })
-      cb(null, balayAudPath)
-    },
-    filename: function (req, file, cb) {
-
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-      console.log(file)
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-      cb(null, Date.now() + file.originalname) //Appending .jpg
-    }
-  })
-
-  const upload = multer({ storage: storage }).single('avatar')
-
   const { fname, lname, dob, bio } = req.body;
-  console.log("logging req body " + req.body);
-  var avatar = "monkey";
-  upload(req, res, function (err) {
-    if (err instanceof multer.MulterError) {
-      console.log('A Multer error occurred when uploading.')
-    } else if (err) {
-
-      console.log(err)
-      console.log('An unknown error occurred when uploading.')
-    }
-    console.log('Everything went fine.')
-  })
-
 
   const profile: Profile = {
     fname: fname,
     lname: lname,
     bio: bio,
     userId,
-    avatar: avatar
   };
+  //if file exist on the server
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      var newDirectory = "uploads/" + username;
+      fs.mkdirSync(newDirectory, { recursive: true });
+      cb(null, newDirectory);
+    },
+
+    filename: function (req, file, cb) {
+      cb(null, "profile.jpg"); //Appending .jpg
+      profile.avatar = "profile";
+    },
+  });
+
+  const upload = multer({ storage: storage }).single("avatar");
+
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log("A Multer error occurred when uploading.");
+    } else if (err) {
+      console.log(err);
+      console.log("An unknown error occurred when uploading.");
+    }
+    console.log("Everything went fine.");
+  });
+
   if (!profile.dob) {
     profile.dob = null;
   }
   try {
-    // console.log(profile);
-
     const updated = ProfileService.updateProfile(profile);
 
     return res
-      .status(401)
+      .status(201)
       .json({ succes: true, msg: "updated successfully", updated });
   } catch (error) {
     return res.status(404).json({ error });
